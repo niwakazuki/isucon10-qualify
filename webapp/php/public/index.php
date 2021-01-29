df4b2dd99de7f907b3f1a7cf64b1cb24f7374b18
niwakazuki <niwa@fb.tapweb.jp>
2021-01-30 05:24:48 +0900

    * php-fpmにzipとpdo_mysql必要だったので修正 * スコアは736に

--- webapp/php/public/index.php
+++ webapp/php/public/index.php
@@ -12,7 +12,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();
-
 if (false) { // Should be set to true in production
 	$containerBuilder->enableCompilation(__DIR__ . '/var/cache');
 }
