9ad4b82254e934cbc60ad9c5afbddbcd23aa52bf
niwakazuki <niwa@fb.tapweb.jp>
2021-02-03 02:19:02 +0900

    * index変更。minus_popularityは単体では使われていない。

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -21,11 +21,11 @@ CREATE TABLE isuumo.estate
     popularity  INTEGER             NOT NULL,
     minus_popularity INTEGER GENERATED ALWAYS AS (-popularity),
     PRIMARY KEY (`id`),
-    KEY `krent` (`rent`) USING BTREE,
-    KEY `kdoor_width` (`door_width`) USING BTREE,
-    KEY `kdoor_height` (`door_height`) USING BTREE,
-    SPATIAL KEY `point` (`point`),
-    KEY `kminus_popularity` (`minus_popularity`) USING BTREE
+    KEY `idx_rent` (`rent`) USING BTREE,
+    KEY `idx_door_width` (`door_width`) USING BTREE,
+    KEY `idx_door_height` (`door_height`) USING BTREE,
+    SPATIAL KEY `idx_point` (`point`),
+    KEY `idx_minuspopularity_id` (`minus_popularity`,`id`) USING BTREE
 );
 
 CREATE TABLE isuumo.chair
@@ -45,9 +45,11 @@ CREATE TABLE isuumo.chair
     minus_popularity INTEGER GENERATED ALWAYS AS (-popularity),
     stock       INTEGER         NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `kprice` (`price`) USING BTREE,
-    KEY `kwidth` (`width`) USING BTREE,
-    KEY `kdepth` (`depth`) USING BTREE,
-    KEY `kpopularity` (`popularity`) USING BTREE,
-    KEY `kminus_popularity` (`minus_popularity`) USING BTREE
+    KEY `idx_price` (`price`) USING BTREE,
+    KEY `idx_height` (`height`) USING BTREE,
+    KEY `idx_width` (`width`) USING BTREE,
+    KEY `idx_depth` (`depth`) USING BTREE,
+    KEY `idx_color` (`color`) USING BTREE,
+    KEY `idx_kind` (`kind`) USING BTREE,
+    KEY `idx_minuspopularity_id` (`minus_popularity`,`id`) USING BTREE
 );
